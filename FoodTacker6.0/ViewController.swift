ace850cc9952ae39ead91874c13cf39b6e811d3a
Rollin Francois <Rollin@rollins-macbook-pro.local>
2017-01-12 18:28:58 -0500

    To implement the imagePickerController(_:didFinishPickingMediaWithInfo:) method

--- FoodTacker6.0/ViewController.swift
+++ FoodTacker6.0/ViewController.swift
@@ -42,6 +42,19 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         dismiss(animated: true, completion: nil)
     }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        guard let selectedInmage = info[UIImagePickerControllerOriginalImage] as?
+        UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        // Set photoImageView to display the selected image.
+        photoImageView.image = selectedInmage
+        
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
+    }
+    
     //MARK: Actions
     @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
         // Hide the keyboard
