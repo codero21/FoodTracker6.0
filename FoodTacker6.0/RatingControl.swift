21211db3936ddd1013dfef1ddc5f81f610510a32
Rollin Francois <Rollin@rollins-macbook-pro.local>
2017-01-12 20:08:21 -0500

    To add images to iOS Simulator To create a subclass of UIStackView

---
+++ FoodTacker6.0/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodTacker6.0
+//
+//  Created by Rollin Francois on 1/12/17.
+//  Copyright © 2017 Francois Technologies. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
